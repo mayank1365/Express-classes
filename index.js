2fedfcb1410fe97ac443bab4be3d498b5baae3f7
mayank1365 <mayank1365@gmail.com>
2024-07-03 11:11:45 +0530

    middleWares

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-const express = require('express');
-
+const express= require('express');
 const app = express();
-const port = 3000;
-app.use(express.json());
 
-let courses = [
-    { id : 1 , "name": "course1" },
-    { id : 2 , "name": "course2" },
-    { id : 3 , "name": "course3" },
+app.use(express.json());
+app.use(middleware);
+
+let courses=[
+    {"id":"1", "name":'java'},
+    {"id":"2", "name":'javascript'},
+    {"id":"3", "name":'python'}
 ];
 
-app.get('/courses', (req, res) => {
+app.get('/courses', (req, res)=>{
     res.json(courses);
-});
+})
 
-app.post('/courses', (req, res) => {
+
+
+app.post('/courses', (req, res)=>{
     console.log(req.body);
-    let singleCourse = {
-        id : courses.length + 1,
+    const course={
+        "id": (courses.length + 1).toString(),
         "name": req.body.name
     };
-    courses.push(singleCourse);
-    res.json(courses);
+    courses.push(course);
+    res.send(course);
 });
 
-app.put('/courses/:id', (req, res) => {
-    let course = courses.find(c => c.id === parseInt(req.params.id));
+app.put('/courses/:id', (req, res)=>{
+    const course= courses.find(c=>c.id===req.params.id);
     if(!course) return res.status(404).send('The course with the given ID was not found');
-    course.name = req.body.name;
-    res.json(course);
+    course.name=req.body.name;
+    res.send(course);
 });
 
-app.delete('/courses/:id', (req, res) => {
-    let course = courses.find(c => c.id === parseInt(req.params.id));
+app.delete('/courses/:id', (req, res)=>{
+    const course= courses.find(c=>c.id===req.params.id);
     if(!course) return res.status(404).send('The course with the given ID was not found');
-    let index = courses.indexOf(course);
+    const index= courses.indexOf(course);
     courses.splice(index, 1);
-    res.json(courses);
+    res.send(course);
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+function middleware(req, res, next) {
+    console.log("called");
+    console.log(req.hostname);
+    console.log(req.ip);
+    console.log(req.method);
+    console.log(req.url);
+    console.log(new Date());
+    next();
+}
+
+app.listen(3000, ()=>console.log('listening on port 3000...'));
