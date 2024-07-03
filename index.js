01951d3a034c8021c91998ca62ef9f3245830eaf
mayank1365 <mayank1365@gmail.com>
2024-07-03 09:48:25 +0530

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+let courses = [
+    { id : 1, name : 'course1'},
+    { id : 2, name : 'course2'},
+    { id : 3, name : 'course3'}
+];
+
+app.get('/courses', (req, res) => {
+    res.json(courses);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
